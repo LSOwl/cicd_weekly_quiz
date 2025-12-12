037a1de45e93b088f1aa9ff74907da3eaedb1c7c
LSOwl <gavin.nrjones@gmail.com>
2025-12-12 09:37:37 -0600

    multiplication tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,10 +5,17 @@ root = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(root / "src"))
 
 
-from app import add
+import app
 
 def test_add():
     assert add(5, 6) == 11
-
-def test_add2():
     assert add(5, 6) != 10
+
+def test_sub():
+    assert sub(10, 4) == 6
+    assert sub(5, 22) == -17
+
+def test_mult():
+	assert mult(5, 6) == 30
+	assert mult(0.5, 8) == 4
+	assert mult(-5, 1) == -5
