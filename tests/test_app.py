543b916e8eb892c30767abd707ee6185180ba0fb
LSOwl <gavin.nrjones@gmail.com>
2025-12-12 10:25:44 -0600

    refactored tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,34 +6,80 @@ sys.path.insert(0, str(root / "src"))
 
 from app import *
 
+# ------------------------ #
+# ---- addition tests ---- #
+# ------------------------ #
 def test_add():
     assert add(5, 6) == 11
+
+def test_add1():
     assert add(5, 6) != 10
 
+# --------------------------- #
+# ---- subtraction tests ---- #
+# --------------------------- #
 def test_sub():
     assert sub(10, 4) == 6
+
+def test_sub1():
     assert sub(5, 22) == -17
 
+# ------------------------------ #
+# ---- multiplication tests ---- #
+# ------------------------------ #
 def test_mult():
 	assert mult(5, 6) == 30
+
+def test_mult1():
 	assert mult(0.5, 8) == 4
+
+def test_mult2():
 	assert mult(-5, 1) == -5
 
+# ------------------------ #
+# ---- division tests ---- #
+# ------------------------ #
 def test_div():
-    assert div(10, 5) == 2
+    assert div(10, 5) == 2 
+
+def test_div1():
     assert div(6, 1.5) == 4
+
+def test_div2():
     assert div(10, -2) == -5
 
+# ------------------------- #
+# ---- logarithm tests ---- #
+# ------------------------- #
 def test_log():
     assert log(100) == 2
+
+def test_log1():
     assert log(8, 2) == 3
+
+def test_log2():
     assert log(1) == 0
+
+def test_log3():
     assert log(1, 52) == 0
+
+def test_log4():
     assert log(6, 6) == 1
 
+# ---------------------- #
+# ---- square tests ---- #
+# ---------------------- #
 def test_square():
     assert square(5) == 25
+
+def test_square1():
     assert square(0.5) == 0.25
+
+def test_square2():
     assert square(-4) == 16
+
+def test_square3():
     assert square(0) == 0
+
+def test_square4():
     assert square(1) == 1
