95c8323cae099972ebaeb2869b63bbc242145331
LSOwl <gavin.nrjones@gmail.com>
2025-12-12 09:38:53 -0600

    fixed tests import app

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,8 +4,7 @@ from pathlib import Path
 root = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(root / "src"))
 
-
-import app
+from app import *
 
 def test_add():
     assert add(5, 6) == 11
